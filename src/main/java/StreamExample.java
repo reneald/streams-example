495d3d6c5b8a4d0c2a585b273f08cf5f4a900891
Van Reeth Leander (100) <leander.vanreeth@cm.be>
2018-10-01 10:13:25 +0200

    added studentlist, print statements

--- src/main/java/StreamExample.java
+++ src/main/java/StreamExample.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -13,35 +10,64 @@ public class StreamExample {
     public static void main(String[] args) {
 
         Stream<String> generatedStream = Stream.generate(() -> "example").limit(10);
+        List<String> collect = generatedStream.collect(Collectors.toList());
+        System.out.println(collect);
 
         IntStream intStream = IntStream.range(1, 42); //last element == 41
-        LongStream longStream = LongStream.rangeClosed(1, 41); //last element ==41
+        System.out.println(intStream.reduce((x, y) -> x + y).toString());
 
-        List<Student> students = new ArrayList<>();
-        List<String> studentNamesStartingWithA = students.stream() // source
-                .map(x -> x.getFirstName()) // intermediate operation
-                .filter(firstName -> firstName.startsWith("A")) //intermediate operation
+        LongStream longStream = LongStream.rangeClosed(1, 41); //last element ==41
+        System.out.println(longStream.reduce((x, y) -> x + y).toString());
+        System.out.println(longStream.reduce(10, (x, y) -> x + y));
+
+        List<Student> students = generateStudentList();
+        List<String> studentNamesStartingWithV = students.stream() // source
+                .map(x -> x.getLastName()) // intermediate operation
+                .filter(firstName -> firstName.startsWith("V")) //intermediate operation
                 .collect(Collectors.toList()); // terminal operation
+        System.out.println(studentNamesStartingWithV); // shows that order of items is retained
 
         List<String> shortenedNamesSkippedTwo = students.stream()
                 .skip(2)
                 .map(Student::getLastName)
                 .map(name -> name.substring(0, 2))
                 .collect(Collectors.toList());
+        System.out.println(shortenedNamesSkippedTwo);
 
         Optional<String> optionalNameStartingWithA = students.stream()
                 .map(Student::getLastName)
                 .filter(name -> name.startsWith("A"))
                 .findFirst();
+        System.out.println(optionalNameStartingWithA);
 
         // intermediate operations which reduce the size of the stream
         // should be placed before operations which are applying to each element.
 
-        OptionalInt reduced =
-                IntStream.range(1, 4).reduce((a, b) -> a + b); // without identity(initial value)
+    }
 
-        int reducedTwoParams =
-                IntStream.range(1, 4).reduce(10, (a, b) -> a + b);
+    private static List<Student> generateStudentList() {
+        Student willem = new Student("Willem", "Peeters", 5, 95);
+        Student julien = new Student("Julien", "Serbruyns", 1, 75);
+        Student cedric = new Student("Cedric", "Vanginderachter", 3, 80);
+        Student jan = new Student("Jan", "Van Achte", 10, 85);
+        Student maxime = new Student("Maxime", "Ramaekers", 1, 76);
+        Student jo = new Student("Jo", "Vanpaeschen", 4, 65);
+        Student chiHow = new Student("Chi How", "Hu", 8, 84);
+        Student brecht = new Student("Brecht", "Deschoenmaeker", 7, 52);
+        Student leander = new Student("Leander", "Van Reeth", 1, 75);
+        Student maarten = new Student("Maarten", "Redant", 1, 80);
+        Student paul = new Student("Paul", "McCartney", 2, 74);
+        Student john = new Student("John", "Lennon", 3, 72);
+        Student ringo = new Student("Ringo", "Starr", 2, 81);
+        Student george = new Student("George", "Harrison", 2, 98);
+        Student miley = new Student("Miley", "Cyrus", 1, 83);
+        Student hannah = new Student("Hannah", "Montana", 1, 65);
+        Student gwen = new Student("Gwen", "Stefani", 6, 79);
+        Student liliane = new Student("Liliane", "St-Pierre", 11, 74);
+        Student dana = new Student("Dana", "Winner", 7, 73);
+        Student ann = new Student("Ann", "Christie", 15, 90);
 
+        return Arrays.asList(willem, julien, cedric, jan, maxime, jo, chiHow, brecht, leander, maarten, paul, john, ringo,
+        george, miley, hannah, gwen, liliane, dana, ann);
     }
 }
